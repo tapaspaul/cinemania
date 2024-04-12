2be1872befe43b2ab84da9415a044cacdf8f482c
Tapas Paul <tapaspaul833@gmail.com>
2024-04-12 09:24:47 +0530

    Added Bootstrap, Header, and Footer

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
+import '@/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
+import Header from '@/components/header/header.js';
+import Footer from '@/components/footer/footer.js';
 
 export const metadata = {
   title: "Cinemania"
@@ -7,7 +10,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main id="main_wrapper">
+          <Header></Header>
+          <div id="site_content">
+            {children}
+          </div>
+          <Footer></Footer>
+        </main>
+      </body>
     </html>
   );
 }
