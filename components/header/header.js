ea7d7aeeb17b9ae787b4748ba92923d532de3439
Tapas Paul <tapaspaul833@gmail.com>
2024-04-15 19:16:05 +0530

    Fix header design

--- components/header/header.js
+++ components/header/header.js
@@ -19,6 +19,9 @@ export default function Header(){
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
+                                <NavLink href="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
                                 <NavLink href="/movies">Movies</NavLink>
                             </li>
                             <li className="nav-item">
