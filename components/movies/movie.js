01a4294723fc5b949ea2437799698d0ee016bc50
Tapas Paul <tapaspaul833@gmail.com>
2024-04-30 00:02:12 +0530

    Implement Add to Watchlist Functionality

--- components/movies/movie.js
+++ components/movies/movie.js
@@ -1,6 +1,8 @@
-import { MdVisibility } from 'react-icons/md';
+import { useState } from 'react';
+import { MdPostAdd } from 'react-icons/md';
 import classes from './movies.module.css';
 import Link from 'next/link';
+import { addToWatchlist } from './add-to-watchlist';
 
 const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'short', day: '2-digit' };
@@ -9,6 +11,16 @@ const formatDate = (dateString) => {
 };
 
 export default function Movie({ movie }){
+    const [watchList, setWatchList] = useState([]);
+    const [ error, setError ] = useState(null);
+    const handleWatchlist = async (movieId) => {
+        try{
+            const responseData = await addToWatchlist(movieId);
+            setWatchList([ ...watchList, responseData ]);
+        }catch (error){
+            setError({message: error.message || 'Not able to add movie to the watchlist, Please try again later.'});
+        }
+    }
     return(
         <div className={ `${ classes['movie-content'] } position-relative overflow-hidden` }>
             <img src={`https://image.tmdb.org/t/p/w500/${ movie.poster_path }`} alt={movie.title} className="img-fluid transition-6" />
@@ -17,8 +29,8 @@ export default function Movie({ movie }){
                     <Link href={`/movies/${ movie.id }`} className="text-white">{ movie.title }</Link>
                 </h4>
                 <p className={ classes.date }>{ formatDate( movie.release_date) }</p>
-                <button type="button" className="btn btn-sm btn-outline-primary d-flex align-items-center justify-content-center gap-2 w-100 mt-3">
-                    <MdVisibility className="d-block" />
+                <button onClick={() => handleWatchlist(movie.id)} type="button" className="bg-transparent text-primary p-0 d-flex align-items-center gap-2 w-100 mt-3 border-0">
+                    <MdPostAdd className="d-block" />
                     <span>Add to Watchlist</span>
                 </button>
             </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        TMDB_API_KEY: '1829846e13ce79eb8bb3f9657075247f'
+        TMDB_API_KEY: '1829846e13ce79eb8bb3f9657075247f',
+        ACCOUNT_ID: '21211402',
+        AUTH_TOKEN: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxODI5ODQ2ZTEzY2U3OWViOGJiM2Y5NjU3MDc1MjQ3ZiIsInN1YiI6IjY2MWE4MjAyOTY3Y2M3MDE3ZjlmNTJjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wxqWfz08tb40kQC_RN5r2bEEUt3HOVA4SaJ3KRfc1rU'
     }
 };
 
