54a3e6de25329c52d54a81c172728ea6d0b0f216
tap1995 <tapas.paul@codeclouds.in>
2024-04-30 19:54:19 +0530

    Small Changes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import HomeHero from '@/components/home/banner/banner';
+import '@/components/home/home.css';
 import Upcoming from "@/components/movies/upcoming";
 import TopRated from "@/components/movies/top-rated";
-import '@/components/home/home.css';
 import NowPlaying from '@/components/movies/now-playing';
 
 export default function Home() {
