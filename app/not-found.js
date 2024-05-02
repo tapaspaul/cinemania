8c2bd00e1ff5c24a0a0ed422b7d0388b31272ac7
Tapas Paul <tapaspaul833@gmail.com>
2024-05-02 08:52:24 +0530

    Added Not Found Page

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound(){
+    return(
+        <div className="not-found">
+            <h1>404</h1>
+            <h3>Not Found</h3>
+            <p>We Could not find the requested page or resources.</p>
+        </div>
+    )
+}
